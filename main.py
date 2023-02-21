0109ea70ce4bc36c5497a288aaebaed019c2ba43
winkCodeGit <wink.code@gmail.com>
2023-02-21 22:20:06 +0530

    new changes

--- main.py
+++ main.py
@@ -1,43 +1,51 @@
-from fastapi import FastAPI,UploadFile
+from fastapi import FastAPI, WebSocket
+from fastapi.responses import HTMLResponse
+
 app = FastAPI()
-from pydantic import BaseModel
-import cv2
-import numpy as np
-import pyzbar.pyzbar as pyzbar
+
+html = """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Chat</title>
+    </head>
+    <body>
+        <h1>WebSocket Chat</h1>
+        <form action="" onsubmit="sendMessage(event)">
+            <input type="text" id="messageText" autocomplete="off"/>
+            <button>Send</button>
+        </form>
+        <ul id='messages'>
+        </ul>
+        <script>
+            var ws = new WebSocket("ws://localhost:8000/ws");
+            ws.onmessage = function(event) {
+                var messages = document.getElementById('messages')
+                var message = document.createElement('li')
+                var content = document.createTextNode(event.data)
+                message.appendChild(content)
+                messages.appendChild(message)
+            };
+            function sendMessage(event) {
+                var input = document.getElementById("messageText")
+                ws.send(input.value)
+                input.value = ''
+                event.preventDefault()
+            }
+        </script>
+    </body>
+</html>
+"""
 
 
+@app.get("/")
+async def get():
+    return HTMLResponse(html)
 
-
-class Item(BaseModel):
-    name: str
-    description: str | None = None
-    price: float
-    tax: str | None = None
-
-# @app.post("/BarCodeImage")
-# async def BarCodeImage(item:Item):
-#     img = cv2.imdecode(np.frombuffer(await file.read(), np.uint8), -1)
-
-#     decodedObjects = pyzbar.decode(image)
-#     for obj in decodedObjects:
-#         # print("Type: QRCODE")
-#         # print("Data:  b'www.copyassignment.com'")
-#         print("Type:", obj.type)
-#         print("Data: ", obj.data, "\n")
-
-
-#     cv2.imshow("Frame", image)
-#     cv2.waitKey(0)
-
-@app.post("/barCodeImage/")
-async def create_upload_file(file: UploadFile):
-    image = cv2.imdecode(np.frombuffer(await file.read(), np.uint8), -1)
-    decodedObjects = pyzbar.decode(image)
-    data =[]
-    for obj in decodedObjects:
-        data.append({"type":obj.type,"Data":obj.data})
-    
-    # cv2.imshow("Frame", image)
-    # cv2.waitKey(0)
-    return data
-        
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    print("===================")
+    await websocket.accept()
+    while True:
+        data = await websocket.receive_text()
+        await websocket.send_text(f"Message text was: {data}")
